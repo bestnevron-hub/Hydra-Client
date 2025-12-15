9a3885a623408e2f3038170933cc2f3fc1f08d23
bestnevron-hub <bestnevron@gmail.com>
2025-12-15 17:24:05 +0100

    Simplify website to a single product

---
+++ config.py
@@ -0,0 +1,5 @@
+# Токен вашего Telegram-бота
+BOT_TOKEN = '8554588347:AAHhLPDw22IsNDmgFJt1ATrihG9hWCq_IbE'
+
+# Токен платежной системы (пока что заглушка)
+PAYMENT_PROVIDER_TOKEN = 'PASTE_YOUR_PAYMENT_PROVIDER_TOKEN_HERE'
